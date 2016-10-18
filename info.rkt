2225941f8ff49e1aa113c8dcacacfcf2b4a49b8a
Matthias Felleisen <matthias@ccs.neu.edu>
2016-10-18 11:57:43 -0400

    fixed dep

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "web-io")
-(define deps '("racket" "xml" "net" "htdp-lib" "rackunit-lib"))
-(define build-deps '("scribble-lib" "xml" "racket-doc"))
+(define deps '("racket" "net-lib" "htdp-lib" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/web-io.scrbl" ())))
 (define pkg-desc "a supplementary package for Fundamentals I @ NEU, 2016")
 (define version "0.1")
